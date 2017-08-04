b4ab42237a31d5bcb66d6eee62ff01014202ded4
Hector Rodriguez <giovanni.rodriguez@zazil.net>
2017-08-04 11:22:03 -0500

    GioW10: Viernes 4, Agosto 2017: Commit de Mantenimiento

--- application/modules/biblioteca/controllers/UsuarioController.php
+++ application/modules/biblioteca/controllers/UsuarioController.php
@@ -18,8 +18,15 @@ class Biblioteca_UsuarioController extends Zend_Controller_Action
         // action body
     }
 
+    public function logoutAction()
+    {
+        // action body
+    }
+
 
 }
+
+
 
 
 
